b1c1a757a17227f9b576c842112a0822ff3be546
mahdihajizadehofficial <75172352+mahdihajizadehofficial@users.noreply.github.com>
2020-12-17 06:43:53 -0800

    Update and rename file.php to index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
+
+
 include 'madeline.php';
 
 $MadelineProto = new \danog\MadelineProto\API('session.madeline');
