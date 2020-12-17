13aa8e438677aebc35c49e4e2003a450112c7434
mahdihajizadehofficial <75172352+mahdihajizadehofficial@users.noreply.github.com>
2020-12-17 06:35:15 -0800

    Update file.php

--- file.php
+++ file.php
@@ -1,104 +1,49 @@
+
 <?php
 
-ini_set('error_logs','off');
-error_reporting(0);
 if (!file_exists('madeline.php')) {
-copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
-
-
-
-
-$settings =
-	[
-		'logger' => [
-			'logger' => 0
-		],
-		'app_info' => [
-				'device_model' => 'file',
-				'system_version' => ''.rand(1,10),
-				'app_version' => 'No Proxy',
-				'lang_code' => 'fa',
-				'api_id' => 809155
-,
-				'api_hash' => 'e586a1d6564778b285d32aa0b3e16218'
-		],
-	];
 include 'madeline.php';
-function closeConnection($message = 'Installed ! <br> @OGHAB_TM')
-{
-if (php_sapi_name() === 'cli' || isset($GLOBALS['exited'])) {
-        return;
-  }
-    @ob_end_clean();
-    header('Connection: close');
-    ignore_user_abort(true);
-    ob_start();
-    echo '<html><body><h1>'.$message.'</h1></body</html>';
-    $size = ob_get_length();
-    header("Content-Length: $size");
-    header('Content-Type: text/html');
-    ob_end_flush();
-    flush();
-    $GLOBALS['exited'] = true;
-}
-function shutdown_function($lock)
-{
-    $a = fsockopen((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'tls' : 'tcp').'://'.$_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT']);
-    fwrite($a, $_SERVER['REQUEST_METHOD'].' '.$_SERVER['REQUEST_URI'].' '.$_SERVER['SERVER_PROTOCOL']."\r\n".'Host: '.$_SERVER['SERVER_NAME']."\r\n\r\n");
-    flock($lock, LOCK_UN);
-    fclose($lock);
-}
 
-if (!file_exists('bot.lock')) {
-    touch('bot.lock');
-}
-$lock = fopen('bot.lock', 'r+');
-
-$try = 1;
-$locked = false;
-while (!$locked) {
-    $locked = flock($lock, LOCK_EX | LOCK_NB);
-    if (!$locked) {
-        closeConnection();
-
-        if ($try++ >= 30) {
-            exit;
-        }
-        sleep(1);
-    }
-}
-//End
-//@OGHAB_tm
-//StartMadeLine :
-$MadelineProto = new \danog\MadelineProto\API('oghab.madeline',$settings);
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
 $MadelineProto->start();
-$offset = -2;
-register_shutdown_function('shutdown_function', $lock);
-closeConnection();
+$time=date("H:i",strtotime("5 hour"));
+$sana = date('d-m-Y',strtotime('5 hour'));
+$MadelineProto->account->updateProfile(['first_name'=>"$time #KoderNet",'about'=>"👨‍💻 #KoderNet vaqti:⌚️ $time 📆Sana: $sana 📡Kanalim: @Php_Koderlar"]);
+$Bool = $MadelineProto->account->updateStatus(['offline' => false ]);
 while (true) {
-    $updates = $MadelineProto->get_updates(['offset' => $offset, 'limit' => 50, 'timeout' => 0]);
+    $updates = $MadelineProto->get_updates(['offset' =>'-1', 'limit' => 10, 'timeout' => 0]); // Just like in the bot API, you can specify an offset, a limit and a timeout
     \danog\MadelineProto\Logger::log($updates);
     foreach ($updates as $update) {
-        $offset = $update['update_id'] + 1;
-     $up = $update['update']['_'];
-                if ($up == 'updateNewMessage' or $up == 'updateNewChannelMessage') {
+        $offset = $update['update_id'] + 1; // Just like in the bot API, the offset must be set to the last update_id
+        switch ($update['update']['_']) {
+            case 'updateNewMessage':
                 if (isset($update['update']['message']['out']) && $update['update']['message']['out']) {
                     continue;
                 }
-  $chatID = $MadelineProto->get_info($update['update']);
-  $type = $chatID['type'];
-  $chatID = $chatID['bot_api_id'];
-  $userID = $update['update']['message']['from_id'];
-  $msg = $update['update']['message']['message'];
-  $msg_id = $update['update']['message']['id'];
-
-   try{
-require 'dl.php';
-   }
-   catch (\danog\MadelineProto\RPCErrorException $e) {
+                $res = json_encode($update, JSON_PRETTY_PRINT);
+                if ($res == '') {
+                    $res = var_export($update, true);
+                }
+                $text=$update['update']['message']['message'];
+                if(mb_stripos($text,'Salom')!==false or mb_stripos($text,'salom')!==false or mb_stripos($text,'Assalomu alekum')!==false){
+                    try {
+                        $MadelineProto->messages->sendMessage([
+                            'peer' => $update['update']['message']['from_id'], 
+                            'message' => "👋Assalomu alekum,bu avto javob qaytargichim
+⏰Mening ish vaqtim: 13:00 - 21:00 
+⚠Agar online bo'lmasam xabaringizni qoldiring 24 Soat ichida o'zim javob beraman!",
+                            'reply_to_msg_id' => $update['update']['message']['id'],
+                            'entities' => [['_' => 'messageEntityPre', 'offset' => 0, 'length' => strlen($res), 'language' => 'json']]]);
+                    
+                    } catch (\danog\MadelineProto\RPCErrorException $e) {
+                    $MadelineProto->messages->sendMessage(['peer' => '@KoderNet', 'message' => $e->getCode().': '.$e->getMessage().PHP_EOL.$e->getTraceAsString()]);
+                        
+                    }
+                }
+        }
+    }
 }
-}
-}
-}
+?>
 
